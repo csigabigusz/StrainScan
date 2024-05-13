4e76df3818885c85b2c5ecdb93eeae76d94d4520
Csilla Lehoczky <csilla.lehoczky@medipredict.com>
2024-05-13 13:56:17 +0000

    correct error

--- library/Build_tree_mem.py
+++ library/Build_tree_mem.py
@@ -97,7 +97,7 @@ def hierarchy(fna_mapping, dist):
 #def extract_kmers(fna_i, fna_path, ksize, kmer_index_dict, kmer_index, Lv, spec, tree_dir, alpha_ratio, identifier):
 def extract_kmers(fna_i, fna_path, ksize, kmer_index_dict, Lv, spec, alpha_ratio, identifier):
     print('Start extract_kmers function' + u'- Current Memory Usage: %.4f GB' % (psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024 / 1024))
-    print('kmer index:',kmer_index, 'length of kmer_index_dict:', len(kmer_index_dict))
+    #print('kmer index:',kmer_index, 'length of kmer_index_dict:', len(kmer_index_dict))
     kmer_index=len(kmer_index_dict)+1
     #print('kmer_index_dict', len(kmer_index_dict),  sys.getsizeof(pkl.dumps(kmer_index_dict)))
     kmer_sta = defaultdict(int)
